de3407a512bf52e89d366427eebdd25649bc3714
Nicolas MANOSALVA PEREZ <niman@psb.ugent.be>
2023-06-23 09:42:57 +0200

    updated obo and TF-fam file of maize_v5

--- mini_ac.sh
+++ mini_ac.sh
@@ -3,4 +3,4 @@
 
 module load nextflow/x86_64/21.10.6
 
-nextflow -C /group/transreg/niman/MINI-AC_runs/mini_ac.config run /group/transreg/niman/miniac_repo/MINI-AC/mini_ac.nf --species maize_v5 --mode locus_based
+nextflow -C /group/transreg/niman/MINI-AC_runs/mini_ac.config run /group/transreg/niman/miniac_repo/MINI-AC/mini_ac.nf --species maize_v5 --mode locus_based -resume
