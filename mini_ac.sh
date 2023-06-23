29634f539f5e1df7f62a4d53934133b85b813e5d
Nicolas MANOSALVA PEREZ <niman@psb.ugent.be>
2023-06-23 09:47:39 +0200

    remove not ignored file

--- mini_ac.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#$ -l h_vmem=5G
-
-module load nextflow/x86_64/21.10.6
-
-nextflow -C /group/transreg/niman/MINI-AC_runs/mini_ac.config run /group/transreg/niman/miniac_repo/MINI-AC/mini_ac.nf --species maize_v5 --mode locus_based -resume
