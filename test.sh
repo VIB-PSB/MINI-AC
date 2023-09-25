d0e072e694026af22984a9b697172383def9ab7d
Herman De Beukelaer <herman.debeukelaer@psb.ugent.be>
2023-09-25 18:38:50 +0200

    Download test motif mapping files only if not already present

--- test.sh
+++ test.sh
@@ -8,11 +8,17 @@
 
 module load nextflow
 
-# fetch test motif mappings
-curl -k -o tests/data/zma_v4/zma_v4_genome_wide_motif_mappings_chr1.bed \
-           https://floppy.psb.ugent.be/index.php/s/NekMYztyxEnsQiY/download/zma_v4_genome_wide_motif_mappings_chr1.bed
+# fetch test motif mappings (if not already present)
+genome_wide_motif_mapping=tests/data/zma_v4/zma_v4_genome_wide_motif_mappings_chr1.bed
+locus_based_motif_mapping=tests/data/zma_v4/zma_v4_locus_based_motif_mappings_5kbup_1kbdown_chr1.bed
 
-curl -k -o tests/data/zma_v4/zma_v4_locus_based_motif_mappings_5kbup_1kbdown_chr1.bed \
-           https://floppy.psb.ugent.be/index.php/s/r2wQmFjPy79qSp7/download/zma_v4_locus_based_motif_mappings_5kbup_1kbdown_chr1.bed
+if [ ! -f "$genome_wide_motif_mapping" ]; then
+  curl -k -o $genome_wide_motif_mapping https://floppy.psb.ugent.be/index.php/s/NekMYztyxEnsQiY/download/zma_v4_genome_wide_motif_mappings_chr1.bed
+fi
 
+if [ ! -f "$locus_based_motif_mapping" ]; then
+  curl -k -o $locus_based_motif_mapping https://floppy.psb.ugent.be/index.php/s/r2wQmFjPy79qSp7/download/zma_v4_locus_based_motif_mappings_5kbup_1kbdown_chr1.bed
+fi
+
+# run MINI-AC test pipeline
 ~/nf-test test
