f4e604d41ec3358ea3f56b74e005d7eefa365e7e
Herman De Beukelaer <herman.debeukelaer@psb.ugent.be>
2023-09-25 17:47:39 +0200

    Update test scenario for maize genome-wide

--- test.sh
+++ test.sh
@@ -8,4 +8,11 @@
 
 module load nextflow
 
+# fetch test motif mappings
+curl -k -o tests/data/zma_v4/zma_v4_genome_wide_motif_mappings_chr1.bed \
+           https://floppy.psb.ugent.be/index.php/s/NekMYztyxEnsQiY/download/zma_v4_genome_wide_motif_mappings_chr1.bed
+
+curl -k -o tests/data/zma_v4/zma_v4_locus_based_motif_mappings_5kbup_1kbdown_chr1.bed \
+           https://floppy.psb.ugent.be/index.php/s/r2wQmFjPy79qSp7/download/zma_v4_locus_based_motif_mappings_5kbup_1kbdown_chr1.bed
+
 ~/nf-test test
