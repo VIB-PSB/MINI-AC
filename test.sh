248778a5bba0937ead4244a7a6ad1f7ea17228f7
Herman De Beukelaer <herman.debeukelaer@psb.ugent.be>
2023-09-25 18:40:01 +0200

    Rename variables to clarify it's maize

--- test.sh
+++ test.sh
@@ -8,16 +8,16 @@
 
 module load nextflow
 
-# fetch test motif mappings (if not already present)
-genome_wide_motif_mapping=tests/data/zma_v4/zma_v4_genome_wide_motif_mappings_chr1.bed
-locus_based_motif_mapping=tests/data/zma_v4/zma_v4_locus_based_motif_mappings_5kbup_1kbdown_chr1.bed
+# fetch zma_v4 test motif mappings (if not already present)
+zma_v4_genome_wide_motif_mapping=tests/data/zma_v4/zma_v4_genome_wide_motif_mappings_chr1.bed
+zma_v4_locus_based_motif_mapping=tests/data/zma_v4/zma_v4_locus_based_motif_mappings_5kbup_1kbdown_chr1.bed
 
-if [ ! -f "$genome_wide_motif_mapping" ]; then
-  curl -k -o $genome_wide_motif_mapping https://floppy.psb.ugent.be/index.php/s/NekMYztyxEnsQiY/download/zma_v4_genome_wide_motif_mappings_chr1.bed
+if [ ! -f "$zma_v4_genome_wide_motif_mapping" ]; then
+  curl -k -o $zma_v4_genome_wide_motif_mapping https://floppy.psb.ugent.be/index.php/s/NekMYztyxEnsQiY/download/zma_v4_genome_wide_motif_mappings_chr1.bed
 fi
 
-if [ ! -f "$locus_based_motif_mapping" ]; then
-  curl -k -o $locus_based_motif_mapping https://floppy.psb.ugent.be/index.php/s/r2wQmFjPy79qSp7/download/zma_v4_locus_based_motif_mappings_5kbup_1kbdown_chr1.bed
+if [ ! -f "$zma_v4_locus_based_motif_mapping" ]; then
+  curl -k -o $zma_v4_locus_based_motif_mapping https://floppy.psb.ugent.be/index.php/s/r2wQmFjPy79qSp7/download/zma_v4_locus_based_motif_mappings_5kbup_1kbdown_chr1.bed
 fi
 
 # run MINI-AC test pipeline
